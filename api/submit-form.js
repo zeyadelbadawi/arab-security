cae9cfd43baf1f3297364d718cc8746e9aec6afa
Ziad Elbadawi <ziad@Ziads-MacBook-Pro.local>
2026-04-23 17:07:00 +0200

    contact formswds

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,6 +1,6 @@
-const { google } = require("googleapis")
+import { google } from "googleapis"
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" })
   }
